864024de31302dbd36615f4f32b0adf58d82391f
ra07ch <rachidaitmoussa1999@gmail.com>
2023-11-29 09:00:09 +0100

    CPP07 for 1337

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: raitmous <raitmous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:02:17 by raitmous          #+#    #+#             */
-/*   Updated: 2023/11/28 07:02:24 by raitmous         ###   ########.fr       */
+/*   Updated: 2023/11/29 08:58:10 by raitmous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,23 @@
 #include <string>
 #include <iostream>
 
-template<typename T>
-void iter(T *array, int N, void (*func)(T&)) {
+template<typename T, typename T2>
+void iter(T *array, int N, T2) {
 	if (array == NULL || func == NULL)
 		return ;
 	for (int i = 0; i < N; i++) {
 		func(array[i]);
 	}
 }
+
+// template<typename T>
+// void iter(T *array, int N, void (*func)(const T&)) {
+// 	if (array == NULL || func == NULL)
+// 		return ;
+// 	for (int i = 0; i < N; i++) {
+// 		func(array[i]);
+// 	}
+// }
 
 template<typename T>
 void addOne(T& c) {
