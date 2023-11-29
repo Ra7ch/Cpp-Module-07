a7e984103fc1aa2c5bcaa8a5193634d68d73c880
ra07ch <rachidaitmoussa1999@gmail.com>
2023-11-29 09:54:27 +0100

    CPP07 for 1337

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: raitmous <raitmous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:02:17 by raitmous          #+#    #+#             */
-/*   Updated: 2023/11/29 08:58:10 by raitmous         ###   ########.fr       */
+/*   Updated: 2023/11/29 09:42:55 by raitmous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <iostream>
 
 template<typename T, typename T2>
-void iter(T *array, int N, T2) {
+void iter(T *array, int N, T2& func) { 
 	if (array == NULL || func == NULL)
 		return ;
 	for (int i = 0; i < N; i++) {
@@ -25,14 +25,11 @@ void iter(T *array, int N, T2) {
 	}
 }
 
-// template<typename T>
-// void iter(T *array, int N, void (*func)(const T&)) {
-// 	if (array == NULL || func == NULL)
-// 		return ;
-// 	for (int i = 0; i < N; i++) {
-// 		func(array[i]);
-// 	}
-// }
+template<typename T>
+void f1(T a)
+{
+	std::cout << a << "\n";	
+}
 
 template<typename T>
 void addOne(T& c) {
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: raitmous <raitmous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:09:24 by raitmous          #+#    #+#             */
-/*   Updated: 2023/11/27 14:30:50 by raitmous         ###   ########.fr       */
+/*   Updated: 2023/11/29 09:52:55 by raitmous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,56 @@ int main () {
     // trying int array
     int a[] = {0, -1, 65, 87};
     
-    iter(a, 4, &addOne);
+    iter(a, 4, addOne<int>);
     for (int i = 0; i < 4; i++) {
         std::cout<< a[i] << std:: endl;
 	}
 
-	// trying char
+	//trying char
 	char b[] = {'R', 'C', 'f', '1', '8'};
 	
-	iter(b, 5, &addOne);
+	iter(b, 5, f1<const int>);
 	for (int i = 0; i < 5; i++) {
 		std::cout<< b[i] << std:: endl;
 	}
 
-	// // trying std:: string
-	// std::string b[] = {"rachid", "rach", "simo"};
+	// trying std:: string
+	std::string c[] = {"rachid", "rach", "simo"};
 	
-	// iter(b, 3, &addOne);
-	// for (int i = 0; i < 3; i++) {
-	// 	std::cout<< b[i] << std:: endl;
-	// }
-	
+	iter(c, 3, addOne<std::string>);
+	for (int i = 0; i < 3; i++) {
+		std::cout<< c[i] << std:: endl;
+	}
 }
+
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
+
+// template< typename T >
+// void print( T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
+
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
+
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
+
+//   return 0;
+// }
